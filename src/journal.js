c7172ee69b0812b539933de96d96df0596adef1a
Alex Adamovic <alexanderadamovic@gmail.com>
2022-01-31 15:12:51 -0800

    add business logic for JournalEntry constructor and prototype methods
    Co-authored-by: Liam Eller <liamthelastson@gmail.com>

---
+++ src/journal.js
@@ -0,0 +1,33 @@
+export function Journal() {
+  this.entries = {};
+}
+
+Journal.prototype.addEntry = function (entry) {
+  this.entries[entry.title] = entry;
+};
+
+export function JournalEntry(title, body) {
+  this.title = title;
+  this.body = body;
+}
+
+JournalEntry.prototype.wordCounter = function () {
+  const words = this.body.split(' ');
+  return words.length;
+};
+
+JournalEntry.prototype.vowelCounter = function () {
+  const letters = this.body.replace(/[%#$@,.!? ]/g,"").split('');
+  console.log(letters);
+  let vowelCount = 0;
+  let consCount = 0;
+  letters.forEach(function(letter) {
+    if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
+      vowelCount += 1;
+    } else {
+      consCount += 1;
+    }
+  });
+  let countArray = [vowelCount, consCount];
+  return countArray;
+};
